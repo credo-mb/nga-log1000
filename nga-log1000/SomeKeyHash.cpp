f274c264e5756fdd8c9f44f97a7f146cb514d3ed
Credo Mbikayi <credombikayi@localhost.localdomain>
2018-03-16 20:34:24 -0400

    Au tour de SomeKey... toujours des {}

--- nga-log1000/SomeKeyHash.cpp
+++ nga-log1000/SomeKeyHash.cpp
@@ -12,8 +12,9 @@ unsigned long SomeKeyHash::hash(const char *str) const {
         unsigned long hash = 5381;
         int c;
 
-        while (c = *str++)
+        while (c = *str++) {
             hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+        }
 
         return hash;
 }
